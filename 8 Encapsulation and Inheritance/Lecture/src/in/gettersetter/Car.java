86444a7398aa72f3cc037dd1823479b9150174f4
PramodkrOfficial <pramodkumarrupauliya@gmail.com>
2024-12-09 07:58:08 +0530

    Learning Phase V

--- 8 Encapsulation and Inheritance/Lecture/src/in/gettersetter/Car.java
+++ 8 Encapsulation and Inheritance/Lecture/src/in/gettersetter/Car.java
@@ -13,15 +13,40 @@ public class Car {
         this.costOfPurchase = costOfPurchase;
     }
 
+    public double getFuelLevel() {
+        return fuelLevel;
+    }
+
+    public void setFuelLevel(double fuelLevel) {
+        this.fuelLevel = fuelLevel;
+    }
+
     public String getColor() {
         return color;
     }
 
     public void setColor (String color) {
-        this.color = color;
+        if (color.equals("Yellow")) {
+            System.out.println("pagal hai kya");
+        } else {
+            this.color = color;
+        }
     }
 
     public String getModel () {
         return model;
     }
 }
+
+
+/*
+<----------------------------------------------------{"Using of 'Getter' and 'Setter' method in deep"}----------------------------------------------------->
+
+1.Getters:--------------------> Retrieve private field values, typically named get<FieldName>.
+2.Setters:--------------------> Set or update private field values, usually named set<FieldName>.
+3.Control and Validation:-----> Offer controlled access and allow for validation logic.
+4.Encapsulation:--------------> Facilitate read-only or write-only access to class fields.
+5.Flexibility:----------------> Allow for internal changes without affecting external interfaces.
+
+
+ */
--- 8 Encapsulation and Inheritance/Lecture/src/in/knowledgegate/GetterTest.java
+++ 8 Encapsulation and Inheritance/Lecture/src/in/knowledgegate/GetterTest.java
@@ -7,7 +7,8 @@ public class GetterTest {
     public static void main(String[] args) {
         Car car = new Car("Pearl","Volvo",4,8000000);
         car.setColor("Blue");
-        System.out.printf("%s %s",car.getColor(), car.getModel());
+        car.setFuelLevel(5);
+        System.out.printf("%s %s %s",car.getColor(), car.getModel(), car.getFuelLevel());
 
     }
 }
